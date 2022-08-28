65676f4b4a6e513e9a6a1493d8b7e9be703c88b5
LE <feelgoood98@gmail.com>
2022-08-28 19:51:40 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
-
+  get "static_pages/contact"
   # Defines the root path route ("/")
-  root "application#hello"
+  root "static_pages#home"
 end
