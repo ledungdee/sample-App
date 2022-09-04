de3a0dd1393bae726f69cbac485c8dc9d320be1b
LE <feelgoood98@gmail.com>
2022-09-05 00:10:28 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
     @user=User.find(params[:id]) 
   end
   def create 
-    @user=User.new(user_params)
+    @user= User.new(user_params)
     if @user.save 
+      reset_session #Listing 8.40: Logging in the user upon signup.
+      log_in @user  #Listing 8.40: Logging in the user upon signup.
       flash[:success] ="Welcome to the Sample App!"
       redirect_to @user # chuyen huong den ho so khi nguoi dung moi dc tao
       # Handle a successful save. 
