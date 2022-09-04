77e8ad11de55c9cc11597a8432a9d77ee37d09ce
LE <feelgoood98@gmail.com>
2022-09-04 15:13:13 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new 
   end
+  def show
+    @user=User.find(params[:id]) 
+  end
+  def create 
+    @user=User.new(user_params)
+    if @user.save 
+      flash[:success] ="Welcome to the Sample App!"
+      redirect_to @user # chuyen huong den ho so khi nguoi dung moi dc tao
+      # Handle a successful save. 
+    else
+      render 'new', status: :unprocessable_entity 
+    end 
+  end
+  private
+    def  user_params 
+        params.require(:user).permit(:name, :email, :password, :password_confirmation) 
+    end
 end
